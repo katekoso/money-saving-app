428a48a2af43885133d0658358316e071acf3e83
Nataly <evdokimenkond@yandex.ru>
2023-12-14 18:48:56 +0300

    Navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,7 +1,135 @@
 import "./Navigation.scss";
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+import plus from '../../images/Plus.svg'
+import logoHomeDef from '../../images/HomeDefault.svg'
+import logoHomeAct from '../../images/HomeHover.svg'
+import logoTargetDef from '../../images/TargetDefault.svg'
+import logoTargetAct from '../../images/TargetHover.svg'
+import logoSettingDef from '../../images/SettingDefault.svg'
+import logoSettingAct from '../../images/SettingHover.svg'
+import logoHelpDef from '../../images/HelpDefault.svg'
+import logoHelpAct from '../../images/HelpHover.svg'
+import logOut from '../../images/Logout.svg'
 
 const NavigationComponent: React.FC = () => {
-  return <>Navigation</>;
+
+const [isActivHome, setIsActivHome] = useState(true);
+const [isActivTarget, setIsActivTarget] = useState(false);
+const [isActivSetting, setIsActivSetting] = useState(false);
+const [isActivHelp, setIsActivHelp] = useState(false);
+
+const handlLogoNewTarget = () => {
+  setIsActivHome(false);
+  setIsActivTarget(false);
+  setIsActivSetting(false);
+  setIsActivHelp(false);
+}
+
+const handlLogoHome = () => {
+  setIsActivHome(true);
+  setIsActivTarget(false);
+  setIsActivSetting(false);
+  setIsActivHelp(false);
+}
+
+const handlLogoTarget = () => {
+  setIsActivHome(false);
+  setIsActivTarget(true);
+  setIsActivSetting(false);
+  setIsActivHelp(false);
+}
+
+const handlLogoSetting = () => {
+  setIsActivHome(false);
+  setIsActivTarget(false);
+  setIsActivSetting(true);
+  setIsActivHelp(false);
+}
+
+const handlLogoHelp = () => {
+  setIsActivHome(false);
+  setIsActivTarget(false);
+  setIsActivSetting(false);
+  setIsActivHelp(true);
+}
+
+const handlLogoNewLogOut = () => {
+  setIsActivHome(false);
+  setIsActivTarget(false);
+  setIsActivSetting(false);
+  setIsActivHelp(false);
+}
+
+  return (
+    <div className="navigation">
+      <div className="navigation__logo"></div>
+      <nav>
+        <ul className="navigation_ul">
+          <li>
+            <Link to="/newtarget"
+              onClick={handlLogoNewTarget}>
+              <img
+                src={plus}
+                alt='лого добавления цели'
+                className="navigation__icon navigation__icon_name_plus"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="/"
+              onClick={handlLogoHome}>
+              <img
+                src={`${isActivHome ? logoHomeAct : logoHomeDef}`}
+                alt='лого главной страницы'
+                className="navigation__ikon"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="/target"
+              onClick={handlLogoTarget}>
+              <img
+                src={`${isActivTarget ? logoTargetAct : logoTargetDef}`}
+                alt='лого страницы целей'
+                className="navigation__ikon"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="/settings"
+              onClick={handlLogoSetting}>
+              <img
+                src={`${isActivSetting ? logoSettingAct : logoSettingDef}`}
+                alt='лого настроек'
+                className="navigation__ikon navigation__icon_name_setting"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="/help"
+              onClick={handlLogoHelp}>
+              <img
+                src={`${isActivHelp ? logoHelpAct : logoHelpDef}`}
+                alt='лого помощь'
+                className="navigation__ikon"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="/login"
+              onClick={handlLogoNewLogOut}>
+              <img
+                src={logOut}
+                alt='разлогиниться'
+                className="navigation__ikon"
+              />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default NavigationComponent;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreateGoalPage from "./pages/CreateGoal";
 import GoalPage from "./pages/Goal";
@@ -10,18 +10,16 @@ import TargetPage from "./pages/Target";
 
 const RoutesComponent: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/newtarget" element={<CreateGoalPage />} />
-        <Route path="/target/?card=id" element={<GoalPage />} />
-        <Route path="/help" element={<HelpPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/target" element={<TargetPage />} />
-        <Route />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/newtarget" element={<CreateGoalPage />} />
+      <Route path="/target/?card=id" element={<GoalPage />} />
+      <Route path="/help" element={<HelpPage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
+      <Route path="/target" element={<TargetPage />} />
+      <Route />
+    </Routes>
   );
 };
 
