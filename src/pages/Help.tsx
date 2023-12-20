0cddd09c59a751cf6f5bb76fa37e0b305b5d8880
Nataly <evdokimenkond@yandex.ru>
2023-12-20 13:08:15 +0300

    page help

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Help.scss';
+import HelpItem from "../components/HelpItem/HelpItem";
 import stepOne from '../images/HelpStepOne.jpg'
 import stepTwo from '../images/HelpStepTwo.jpg'
 import stepThree from '../images/HelpStepThree.jpg'
@@ -8,26 +9,30 @@ import stepFour from '../images/HelpStepFour.jpg'
 const HelpPage: React.FC = () => {
   return (
     <ol className="help">
-      <li className="help__item">
-        <h2 className="help__title">Шаг 1</h2>
-        <img src={stepOne} alt="смартфон" />
-        <p className="help__discription">Войдите на платформу Pay and Save</p>
-      </li>
-      <li className="help__item">
-        <h2 className="help__title">Шаг 2</h2>
-        <img src={stepTwo} alt="мишень" />
-        <p className="help__discription">Создайте цель</p>
-      </li>
-      <li className="help__item">
-        <h2 className="help__title">Шаг 3</h2>
-        <img src={stepThree} alt="деньги в мешках" />
-        <p className="help__discription">Настройте списание средств</p>
-      </li>
-      <li className="help__item">
-        <h2 className="help__title">Шаг 4</h2>
-        <img src={stepFour} alt="страла в центре мишени" />
-        <p className="help__discription">Выполните цель</p>
-      </li>
+      <HelpItem 
+        title="Шаг 1"
+        image={stepOne}
+        alt="смартфон"
+        discription="Войдите на платформу Pay and Save"
+      />
+      <HelpItem 
+        title="Шаг 2"
+        image={stepTwo}
+        alt="мишень"
+        discription="Создайте цель"
+      />
+      <HelpItem 
+        title="Шаг 3"
+        image={stepThree}
+        alt="деньги в мешках"
+        discription="Настройте списание средств"
+      />
+      <HelpItem 
+        title="Шаг 4"
+        image={stepFour}
+        alt="страла в центре мишени"
+        discription="Выполните цель"
+      />
     </ol>
   )
 };
