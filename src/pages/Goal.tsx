aed25c9255c2daa8fc018a2b34e51cb4e43cedda
aida2603 <aida.aitenova@yahoo.com>
2023-12-22 18:24:12 -0500

    adding changes

--- src/pages/Goal.tsx
+++ src/pages/Goal.tsx
@@ -2,17 +2,10 @@ import React from "react";
 import { observer } from "mobx-react";
 import goalStore from "../stores/goalStore";
 
-const GoalPage: React.FC = observer(() => {
-  return (
-    <div>
-      <h2>Goals</h2>
-      <ul>
-        {goalStore.goals.map((goal, index) => (
-          <li key={index}>{goal}</li>
-        ))}
-      </ul>
-    </div>
-  );
-});
+import React from "react";
+
+const GoalPage: React.FC = () => {
+  return <>Goal</>;
+};
 
 export default GoalPage;
