b181ec86fbee7d3f87fffcac9991be00903f6196
Daria <d.a.kireeva@mail.ru>
2023-12-25 01:48:56 +0300

    add strapi

---
+++ money/src/api/money/controllers/money.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * money controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::money.money');
---
+++ money/src/api/money/services/money.ts
@@ -0,0 +1,7 @@
+/**
+ * money service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::money.money');
